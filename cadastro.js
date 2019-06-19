afc1104d8690ccf3faee1d03b385f6ff74c49406
Alessandra Lima <alessandramsouzalima@gmail.com>
2019-06-19 12:36:04 -0300

    Atualizei arquivos desde o repl

--- cadastro.js
+++ cadastro.js
@@ -2,35 +2,35 @@
 // Esperar ao html ser carregado
 $(document).ready(function() {
 
-    $('#publicar-post').on('click', function() {
+  $('#publicar-post').on('click', function() {
 
-        // Peguei os valores que o usuario escreveu
-        var autorPost = $('#autor-post').val();
-        var tituloPost = $('#titulo-post').val();
-        var urlImagem = $('#imagem-post').val();
-        var conteudoPost = $('#conteudo-post').val();
+      // Peguei os valores que o usuario escreveu
+      var autorPost = $('#autor-post').val();
+      var tituloPost = $('#titulo-post').val();
+      var urlImagem = $('#imagem-post').val();
+      var conteudoPost = $('#conteudo-post').val();
 
-        var novopost = {
-            "destino": tituloPost,
-            "imagem": urlImagem,
-            "autor": autorPost,
-            "data": new Date().toDateString(),
-            "conteudo": conteudoPost,
-            "curtidas": 0,
-            "comentarios": []
-        };
+      var novopost = {
+          "destino": tituloPost,
+          "imagem": urlImagem,
+          "autor": autorPost,
+          "data": new Date().toDateString(),
+          "conteudo": conteudoPost,
+          "curtidas": 0,
+          "comentarios": []
+      };
 
-        // Pegar blog do localStorage
-        var blog = JSON.parse(localStorage.getItem('blog'));
+      // Pegar blog do localStorage
+      var blog = JSON.parse(localStorage.getItem('blog'));
 
-        // Unshift coloca o elemento na primeira posição do array
-        blog.postagens.unshift(novopost);
+      // Unshift coloca o elemento na primeira posição do array
+      blog.postagens.unshift(novopost);
 
-        // Salvar novo post no local storage
-        localStorage.setItem('blog', JSON.stringify(blog));
+      // Salvar novo post no local storage
+      localStorage.setItem('blog', JSON.stringify(blog));
 
-        location.href = './index.html';
+      location.href = './index.html';
 
-    });
+  });
 
 });
